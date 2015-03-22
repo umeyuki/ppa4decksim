731b934ec2c2b19c7efb97701e915883a8162307
umeyuki <umeyuki1326+github@gmail.com>
2015-03-22 19:28:21 +0900

    add jsx data

---
+++ jsx/data.jsx
@@ -0,0 +1,27 @@
+exports.Members = [
+  {
+    label:          '早川あおい',
+    roma:           'hayakawaaoi',
+    skills:         [ '緩急' ],
+    special_skills: ['マリンボール'] ,
+    event:          'before'
+  },
+  {
+    label:          '橘みずき',
+    roma:           'tachibanamizuki',
+    skills:         ['クロスファイアー'],
+    special_skills: ['クレッセントムーン'] ,
+    event:          'before'
+  },
+  {
+    label:          '六道聖',
+    roma:           'rikudohijiri',
+    skills:         ['キャッチャー','バント'],
+    special_skills: ['ささやき戦術'] ,
+    event:          'before'
+  }
+];
+
+exports.Combo = [
+  { name: 'テリブルトリオ', members: ['hayakawaaoi', 'tachibanamizuki', 'rikudohijiri']}
+];
