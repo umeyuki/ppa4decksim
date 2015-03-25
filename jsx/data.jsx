277223433a524312e8cc5c3d90d626ba318e0616
umeyuki <umeyuki1326+github@gmail.com>
2015-03-25 21:38:16 +0900

    Update data

--- jsx/data.jsx
+++ jsx/data.jsx
@@ -140,7 +140,7 @@ exports.Members =
     special_skills: ['芸術的流し打ち'],
     event_order: 1,
 		type: 0,
-		match_bonus: false
+		match_bonus: true
 
   },
   {
@@ -154,12 +154,192 @@ exports.Members =
 		match_bonus: 1
   },
   {
-    label: '',
-    value: '',
-    traning: '',
-    skills: ['', '', ''],
-    special_skills: [''],
+    label: '友沢亮',
+    value: 'tomosawaryo',
+    traning: '肩力',
+    skills: ['パワーヒッター', 'アベレージヒッター'],
+    special_skills: ['安打製造機'],
+    event_order: 1,
+		type: 0,
+		match_bonus: false
+  },
+  {
+    label: '蛇島桐人',
+    value: 'hebishimakirito',
+    traning: '守備',
+    skills: ['安定感', 'ローボールヒッター', '守備職人', 'ムード'],
+    special_skills: ['魔術師'],
+    event_order: 1,
+		type: 0,
+		match_bonus: false
+  },
+  {
+    label: '矢部田亜希子',
+    value: 'yabetaakiko',
+    traning: '走塁',
+    skills: ['盗塁', '走塁', '送球', 'バント', '内野安打'],
+    special_skills: ['切り込み隊長'],
+    event_order: 1,
+		type: 1,
+		match_bonus: false
+  },
+  {
+    label: '美藤千尋',
+    value: 'bitochihiro',
+    traning: '打撃',
+    skills: ['アベレージヒッター', '粘り打ち', '逆境', 'チャンス'],
+    special_skills: ['安打製造機'],
+    event_order: 1,
+		type: 1,
+		match_bonus: false
+  },
+  {
+    label: '川星ほむら',
+    value: 'kawahoshihomura',
+    traning: '走塁',
+    skills: ['体当たり', 'ムード', 'チャンス', '固め打ち', '内野安打', '意外性', 'いぶし銀', 'ヘッドスライティング'],
+    special_skills: ['気迫ヘッド'],
     event_order: 0,
+		type: 1,
+		match_bonus: false
+  },
+  {
+    label: '武秀英',
+    value: 'bushuei',
+    traning: '打撃',
+    skills: ['ミート', 'ローボールヒッター', 'チャンス', 'パワーヒッター'],
+    special_skills: ['勝負師'],
+    event_order: 1,
+		type: 0,
+		match_bonus: false
+  },
+  {
+    label: '三森右京',
+    value: 'mitumoriukyo',
+    traning: '走塁',
+    skills: ['盗塁', '走塁', '内野安打'],
+    special_skills: ['電光石火'],
+    event_order: 0,
+		type: 0,
+		match_bonus: false
+  },
+  {
+    label: '三森左京',
+    value: 'mitumorisakyo',
+    traning: '走塁',
+    skills: ['盗塁', '走塁', '内野安打'],
+    special_skills: ['高速ベースラン'],
+    event_order: 1,
+		type: 0,
+		match_bonus: false
+  },
+  {
+    label: '浜崎庄司',
+    value: 'hamasakishoji',
+    traning: 'コントロール',
+    skills: ['尻上がり', '闘志'],
+    special_skills: ['不屈の魂'],
+    event_order: 0,
+		type: 0,
+		match_bonus: false
+  },
+  {
+    label: 'ブロンコ=リー',
+    value: 'bronco=lie',
+    traning: '精神',
+    skills: ['チャンス', 'ケガしにくさ', '流し打ち', '粘り打ち', '意外性'],
+    special_skills: ['アーチスト'],
+    event_order: 0,
+		type: 0,
+		match_bonus: false
+  },
+  {
+    label: '夏野向日葵',
+    value: 'natunohimawari',
+    traning: '守備',
+    skills: ['ローボールヒッター', '意外性'],
+    special_skills: ['ストレート送球'],
+    event_order: 1,
+		type: 1,
+		match_bonus: true
+  },
+  {
+    label: '星井スバル',
+    value: 'hoshiisubaru',
+    traning: '変化球',
+    skills: ['コントロール', '尻上がり', '逃げ球'],
+    special_skills: ['スタードライブ'],
+    event_order: 0,
+		type: 0,
+		match_bonus: true
+  },
+  {
+    label: '宇度幹久',
+    value: 'udomikihisa',
+    traning: '筋力',
+    skills: ['パワーヒッター'],
+    special_skills: ['強心臓', '重戦車'],
+    event_order: 1,
+		type: 0,
+		match_bonus: false
+  },
+  {
+    label: '小田切巧',
+    value: 'odagiritakumi',
+    traning: '守備',
+    skills: ['守備職人', '送球'],
+    special_skills: ['代打の神様', '不屈の精神'],
+    event_order: 0,
+		type: 0,
+		match_bonus: false
+  },
+  {
+    label: '京野小筆',
+    value: 'kyounokofude',
+    traning: false,
+    skills: [],
+    special_skills: ['アーチスト', '安打製造機', '鉄腕', '精密機械'],
+    event_order: 0,
+		type: 2,
+		match_bonus: false
+  },
+  {
+    label: '才賀侑人',
+    value: 'saigayuto',
+    traning: '打撃',
+    skills: ['パワーヒッター', '広角打法', '固め打ち'],
+    special_skills: ['一球入魂'],
+    event_order: 1,
+		type: 0,
+		match_bonus: false
+  },
+  {
+    label: '烏丸剛充',
+    value: 'karasumatakemitsu',
+    traning: 'スタミナ',
+    skills: ['ノビ', '打球反応'],
+    special_skills: ['不屈の魂'],
+    event_order: 0,
+		type: 0,
+		match_bonus: false
+  },
+  {
+    label: '春野千優',
+    value: 'harunochiyu',
+    traning: false,
+    skills: [],
+    special_skills: ['鉄人', '走者釘付'],
+    event_order: 1,
+		type: 2,
+		match_bonus: false
+  },
+  {
+    label: '田中将大',
+    value: 'tanakamasahiro',
+    traning: '球速',
+    skills: ['ピンチ', '尻上がり', '打たれ強さ', 'ノビ', 'キレ', '逃げ球', '奪三振', '打球反応', '勝ち運', '闘志', '威圧感'],
+    special_skills: ['エアロスプリット'],
+    event_order: 1,
 		type: 0,
 		match_bonus: true
   },
@@ -167,80 +347,40 @@ exports.Members =
     label: '',
     value: '',
     traning: '',
-    skills: ['', '', ''],
+    skills: ['', ''],
     special_skills: [''],
     event_order: 0,
 		type: 0,
-		match_bonus: true
+		match_bonus: false
   },
   {
     label: '',
     value: '',
     traning: '',
-    skills: ['', '', ''],
+    skills: ['', ''],
     special_skills: [''],
     event_order: 0,
 		type: 0,
-		match_bonus: true
+		match_bonus: false
   },
   {
     label: '',
     value: '',
     traning: '',
-    skills: ['', '', ''],
+    skills: ['', ''],
     special_skills: [''],
     event_order: 0,
 		type: 0,
-		match_bonus: true
+		match_bonus: false
   },
   {
     label: '',
     value: '',
     traning: '',
-    skills: ['', '', ''],
+    skills: ['', ''],
     special_skills: [''],
     event_order: 0,
 		type: 0,
-		match_bonus: true
-  },
-  {
-    label: '',
-    value: '',
-    traning: '',
-    skills: ['', '', ''],
-    special_skills: [''],
-    event_order: 0,
-		type: 0,
-		match_bonus: true
-  },
-  {
-    label: '',
-    value: '',
-    traning: '',
-    skills: ['', '', ''],
-    special_skills: [''],
-    event_order: 0,
-		type: 0,
-		match_bonus: true
-  },
-  {
-    label: '',
-    value: '',
-    traning: '',
-    skills: ['', '', ''],
-    special_skills: [''],
-    event_order: 0,
-		type: 0,
-		match_bonus: true
-  },
-  {
-    label: '',
-    value: '',
-    traning: '',
-    skills: ['', '', ''],
-    special_skills: [''],
-    event_order: 0,
-		type: 0,
-		match_bonus: true
+		match_bonus: false
   },
 ];
