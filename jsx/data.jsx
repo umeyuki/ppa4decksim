5b3df58c095648a249d5fa67c0cebf5f8ba9a96f
umeyuki <umeyuki1326+github@gmail.com>
2015-03-25 21:00:02 +0900

    update data

--- jsx/data.jsx
+++ jsx/data.jsx
@@ -1,52 +1,15 @@
 exports.Members =
- [
+[
   {
-    label:  '早川あおい',
-    value:   'hayakawaaoi',
-    traning: '変化球',
-    skills: ['緩急'],
-    special_skills: ['マリンボール'],
-    event_order: 1
-  },
-  {
-    label: '橘みずき',
-    value: 'tachibanamizuki',
-    traning: 'コントロール',
-    skills: ['クロスファイアー'],
-    special_skills: ['クレッセントムーン'],
-    event_order: 0
-  },
-  {
-   label: '六道聖',
-   value: 'rikudouhijiri',
-   traning: '守備',
-   skills: ['バント', '送球', 'キャッチャー'],
-   special_skills: ['ささやき戦術'],
-   event_order: 1
-  },
-  {
-    label: '猪狩守',
-    value: 'ikarimamoru',
+    label: 'アンヌ',
+    value: 'annu',
     traning: '球速',
-    skills: ['球速', 'ピンチ', 'リリース'],
-    special_skills: ['ライジングキャノン or ライジングショット'],
-    event_order: 1
-  },
-  {
-    label: '大鐘餅太郎',
-    value: 'okanemochitaro',
-    traning: '守備',
-    skills: ['粘り打ち', 'ハイボールヒッター', '初級'],
-    special_skills: ['左キラー'],
-    event_order: 1
-  },
-  {
-    label: '猪狩進',
-    value: 'ikarisusumu',
-    traning: '球速',
-    skills: ['球速', 'ピンチ', 'リリース'],
-    special_skills: ['ライジングキャノン or ライジングショット'],
-    event_order: 1
+    skills: ['フォーク', '', ''],
+    special_skills: ['怪物球威'],
+    event_order: 1,
+    girl_friend: false,
+		type: 1,
+		match_bonus: false
   },
   {
     label: '明星雪華',
@@ -54,102 +17,230 @@ exports.Members =
     traning: false,
     skills: false,
     special_skills: ['一球入魂', '脅威の切れ味'],
-    event_order: 1
+    event_order: 1,
+    girl_friend: true,
+		type: 2,
+		match_bonus: false
   },
-  /* {
-     label: 'アンヌ',
-     value: '',
-     traning: '',
-     skills: ['', '', ''],
-     special_skills: [''],
-     event_order: 0
-     },
-     {
-     label: '',
-     value: '',
-     traning: '',
-     skills: ['', '', ''],
-     special_skills: [''],
-     event_order: 0
-     },
-     {
-     label: '',
-     value: '',
-     traning: '',
-     skills: ['', '', ''],
-     special_skills: [''],
-     event_order: 0
-     },
-     {
-     label: '',
-     value: '',
-     traning: '',
-     skills: ['', '', ''],
-     special_skills: [''],
-     event_order: 0
-     },
-     {
-     label: '',
-     value: '',
-     traning: '',
-     skills: ['', '', ''],
-     special_skills: [''],
-     event_order: 0
-     },
-     {
-     label: '',
-     value: '',
-     traning: '',
-     skills: ['', '', ''],
-     special_skills: [''],
-     event_order: 0
-     },
-     {
-     label: '',
-     value: '',
-     traning: '',
-     skills: ['', '', ''],
-     special_skills: [''],
-     event_order: 0
-     },
-     {
-     label: '',
-     value: '',
-     traning: '',
-     skills: ['', '', ''],
-     special_skills: [''],
-     event_order: 0
-     },
-     {
-     label: '',
-     value: '',
-     traning: '',
-     skills: ['', '', ''],
-     special_skills: [''],
-     event_order: 0
-     },
-     {
-     label: '',
-     value: '',
-     traning: '',
-     skills: ['', '', ''],
-     special_skills: [''],
-     event_order: 0
-     },
-     {
-     label: '',
-     value: '',
-     traning: '',
-     skills: ['', '', ''],
-     special_skills: [''],
-     event_order: 0
-     },
-     {
-     label: '',
-     value: '',
-     traning: '',
-     skills: ['', '', ''],
-     special_skills: [''],
-     event_order: 0
-     }, */
-  ];
+  {
+    label: '阿畑やすし',
+    value: 'abatayasushi',
+    traning: '変化球',
+    skills: [],
+    special_skills: ['オリジナル変化球'],
+    event_order: 0,
+		type: 0,
+		match_bonus: false
+  },
+  {
+    label: '猪狩進',
+    value: 'ikarisusumu',
+    traning: '守備',
+    skills: ['守備', 'アベレージヒッター', '広角打法', 'キャッチャー'],
+    special_skills: ['球界の頭脳'],
+    event_order: 1,
+		type: 0,
+		match_bonus: false
+  },
+  {
+    label: '猪狩守',
+    value: 'ikarimamoru',
+    traning: '球速',
+    skills: ['球速', 'ピンチ', 'リリース'],
+    special_skills: ['ライジングキャノン or ライジングショット'],
+    event_order: 1,
+		type: 0,
+		match_bonus: false
+  },
+  {
+    label: '大鐘餅太郎',
+    value: 'okanemochitaro',
+    traning: '守備',
+    skills: ['粘り打ち', 'ハイボールヒッター', '初級'],
+    special_skills: ['左キラー'],
+    event_order: 1,
+		type: 0,
+		match_bonus: false
+  },
+  {
+    label: '橘みずき',
+    value: 'tachibanamizuki',
+    traning: 'コントロール',
+    skills: ['クロスファイアー'],
+    special_skills: ['クレッセントムーン'],
+    event_order: 0,
+		type: 1,
+		match_bonus: false
+  },
+  {
+    label:  '早川あおい',
+    value:   'hayakawaaoi',
+    traning: '変化球',
+    skills: ['緩急'],
+    special_skills: ['マリンボール'],
+    event_order: 1,
+		type: 1,
+		match_bonus: false
+  },
+  {
+		label: '六道聖',
+		value: 'rikudouhijiri',
+		traning: '守備',
+		skills: ['バント', '送球', 'キャッチャー'],
+		special_skills: ['ささやき戦術'],
+		event_order: 1,
+		type: 0,
+		match_bonus: false
+  },
+  {
+    label: '山口賢',
+    value: 'yamaguchisatoshi',
+    traning: '変化球',
+    skills: ['対ピンチ', '勝ち運'],
+    special_skills: ['強新三'],
+    event_order: 0,
+		type: 0,
+		match_bonus: false
+  },
+  {
+    label: '鈴本大輔',
+    value: 'suzumotodaisuke',
+    traning: 'コントロール',
+    skills: ['対左打者', '打たれ強さ', 'ノビ', 'クリック', '調子安定', '牽制', 'リリース', '打球反応', '球持ち', '低め'],
+    special_skills: ['精密機械'],
+    event_order: 0,
+		type: 0,
+		match_bonus: true
+  },
+  {
+    label: '滝本太郎',
+    value: 'takimototaro',
+    traning: '筋力',
+    skills: ['パワーヒッター', 'アベレージヒッター', '広角打法', '体当たり'],
+    special_skills: ['アーチスト or 安打製造機'],
+    event_order: 0,
+		type: 0,
+		match_bonus: false
+  },
+  {
+    label: '青葉春人',
+    value: 'aobaharuto',
+    traning: '球速',
+    skills: ['重い球', 'キレ', '対強打者', '奪三振', '対ピンチ', '闘志'],
+    special_skills: ['超真魔球改'],
+    event_order: 0,
+		type: 0,
+		match_bonus: false
+  },
+  {
+    label: '佐久間博',
+    value: 'sakumahiroshi',
+    traning: '打撃',
+    skills: ['アベレージヒッター', '内野安打', '流し打ち'],
+    special_skills: ['芸術的流し打ち'],
+    event_order: 1,
+		type: 0,
+		match_bonus: false
+
+  },
+  {
+    label: '樽本有太',
+    value: 'tarumotoyuta',
+    traning: '球速',
+    skills: ['重い球', 'ノビ'],
+    special_skills: ['怪物球威'],
+    event_order: 1,
+		type: 0,
+		match_bonus: 1
+  },
+  {
+    label: '',
+    value: '',
+    traning: '',
+    skills: ['', '', ''],
+    special_skills: [''],
+    event_order: 0,
+		type: 0,
+		match_bonus: true
+  },
+  {
+    label: '',
+    value: '',
+    traning: '',
+    skills: ['', '', ''],
+    special_skills: [''],
+    event_order: 0,
+		type: 0,
+		match_bonus: true
+  },
+  {
+    label: '',
+    value: '',
+    traning: '',
+    skills: ['', '', ''],
+    special_skills: [''],
+    event_order: 0,
+		type: 0,
+		match_bonus: true
+  },
+  {
+    label: '',
+    value: '',
+    traning: '',
+    skills: ['', '', ''],
+    special_skills: [''],
+    event_order: 0,
+		type: 0,
+		match_bonus: true
+  },
+  {
+    label: '',
+    value: '',
+    traning: '',
+    skills: ['', '', ''],
+    special_skills: [''],
+    event_order: 0,
+		type: 0,
+		match_bonus: true
+  },
+  {
+    label: '',
+    value: '',
+    traning: '',
+    skills: ['', '', ''],
+    special_skills: [''],
+    event_order: 0,
+		type: 0,
+		match_bonus: true
+  },
+  {
+    label: '',
+    value: '',
+    traning: '',
+    skills: ['', '', ''],
+    special_skills: [''],
+    event_order: 0,
+		type: 0,
+		match_bonus: true
+  },
+  {
+    label: '',
+    value: '',
+    traning: '',
+    skills: ['', '', ''],
+    special_skills: [''],
+    event_order: 0,
+		type: 0,
+		match_bonus: true
+  },
+  {
+    label: '',
+    value: '',
+    traning: '',
+    skills: ['', '', ''],
+    special_skills: [''],
+    event_order: 0,
+		type: 0,
+		match_bonus: true
+  },
+];
