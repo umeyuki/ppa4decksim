31219d9889fabb609f4ff4b388fef78bfe6ee44a
umeyuki <umeyuki1326+github@gmail.com>
2015-04-09 22:24:32 +0900

    Update 2015-04-09T13:24:21.968Z

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -7,14 +7,18 @@ var App = React.createClass({displayName: "App",
     return {
       options: DATA['Members'],
       members: [],
+      combos: [],
       before_event_count: 0,
       after_event_count:  0,
+      match_bonus: 0
     };
   },
   onLabelClick: function (data, event) {
     console.log(data);
   },
   onChange: function (value, members) {
+    exists = {};
+
     if ( members.length < 6 ) {
       this.setState({ options: DATA['Members']});
     } else {
@@ -23,15 +27,38 @@ var App = React.createClass({displayName: "App",
     this.setState({ members: members } );
     before=0;
     after=0;
+    match_bonus = 0;
+    member_values = [];
     for (var i=0; i < members.length; i++) {
+      member_values.push(members[i]['value']);
       if ( members[i].event_order == 0 ) {
         before++;
       } else {
         after++;
       }
+      if ( members[i].match_bonus ) {
+        match_bonus = match_bonus + 10;
+      }
     }
     this.setState( { before_event_count: before } );
     this.setState( { after_event_count: after  } );
+    this.setState( { match_bonus: match_bonus } );
+    combos = [];
+    for (var i=0; i < DATA['Combos'].length; i++) {
+      combo = DATA['Combos'][i];
+      combo_members = Object.keys(combo['members']);
+      compare = [];
+      for (var j=0; j < combo_members.length; j++) {
+        index =  member_values.indexOf(combo_members[j])
+        if ( index != -1 ) {
+          compare.push(member_values[index]);
+        }
+      }
+      if ( combo_members.toString() == compare.toString() ) {
+        combos.push(combo);
+      }
+    }
+    this.setState({ combos: combos } );
   },
   render: function() {
     var cx = React.addons.classSet;
@@ -41,21 +68,32 @@ var App = React.createClass({displayName: "App",
       onOptionLabelClick: this.onLabelClick, 
       value: this.state.members, 
       multi: true, 
-      noResultsText: "イベキャラ選択が完了しました", 
+      noResultsText: "", 
       placeholder: "イベキャラを選択してください", 
       options: this.state.options, 
       onChange: this.onChange}), 
-    React.createElement("section", {className: "event-count"}, 
+    React.createElement("section", {className: "combo"}, 
+       this.state.combos.length > 0 ? React.createElement("h3", {className: "headline"}, "コンボ発生") : '', 
+      this.state.combos.map(function(combo) {
+        return React.createElement("p", {className: "pure-alert pure-alert-error"}, combo.name);
+      })
+    ), 
+    React.createElement("section", {className: "option"}, 
         React.createElement("span", {className: "pure-button before"}, 
             "前イベ ", this.state.before_event_count
         ), 
         React.createElement("span", {className: "pure-button after"}, 
             "後イベ ", this.state.after_event_count
         ), 
-				React.createElement("div", null, 
-						React.createElement("a", {href: "http://pawapurolabo.tumblr.com/post/115213099093"}, "前イベ・後イベとは?")
-				)
+        React.createElement("span", {className: "pure-button before"}, 
+            "試合経験点ボーナス ",  this.state.match_bonus, " %"
+        ), 
+        React.createElement("div", null, 
+            React.createElement("a", {href: "http://pawapurolabo.tumblr.com/post/115213099093"}, "前イベ・後イベとは?")
+        )
     ), 
+    React.createElement("section", {className: "event-character"}, 
+     this.state.members.length > 0  ? React.createElement("h3", {className: "headline"}, "イベキャラ") : '', 
     this.state.members.map(function(member) {
       var memberClass  = cx({
         "member": true,
@@ -100,25 +138,26 @@ var App = React.createClass({displayName: "App",
       )
       )
       )
-     }, this), 
-      React.createElement("hr", null), 
-      React.createElement("section", {className: "pure-g hint"}, 
+     }, this)
+    ), 
+    React.createElement("hr", null), 
+    React.createElement("section", {className: "pure-g hint"}, 
         React.createElement("div", {className: "pure-u-1-1"}, 
-          React.createElement("div", null, 
-            React.createElement("span", {className: "pure-badge male"}, "男性"), 
-            React.createElement("span", {className: "pure-badge female"}, "女性"), 
-            React.createElement("span", {className: "pure-badge girlfriend"}, "彼女"), 
-            React.createElement("span", {className: "pure-badge girlfriend-player"}, "選手兼彼女")
-          ), 
-          React.createElement("div", null, 
-            React.createElement("span", {className: "pure-badge before"}, "前イベ"), 
-            React.createElement("span", {className: "pure-badge after"}, "後イベ"), 
-            React.createElement("span", {className: "pure-badge traning"}, "得意練習"), 
-            React.createElement("span", {className: "pure-badge skill"}, "練習コツ"), 
-            React.createElement("span", {className: "pure-badge special-skill"}, "金特・オリジナル変化球")
-          )
+            React.createElement("div", null, 
+                React.createElement("span", {className: "pure-badge male"}, "男性"), 
+                React.createElement("span", {className: "pure-badge female"}, "女性"), 
+                React.createElement("span", {className: "pure-badge girlfriend"}, "彼女"), 
+                React.createElement("span", {className: "pure-badge girlfriend-player"}, "選手兼彼女")
+            ), 
+            React.createElement("div", null, 
+                React.createElement("span", {className: "pure-badge before"}, "前イベ"), 
+                React.createElement("span", {className: "pure-badge after"}, "後イベ"), 
+                React.createElement("span", {className: "pure-badge traning"}, "得意練習"), 
+                React.createElement("span", {className: "pure-badge skill"}, "練習コツ"), 
+                React.createElement("span", {className: "pure-badge special-skill"}, "金特・オリジナル変化球")
+            )
         )
-      )
+    )
     )
   }
 });
@@ -35079,6 +35118,283 @@ exports.Members =
     type: 0,
     match_bonus: false
   }
+];
+
+exports.Combos =
+[
+  {
+    name: 'あこがれの大西さん',
+    members:
+    {
+      onishiharison: true,
+      arashimarushiro: true
+    }
+  },
+  {
+    name: '因縁',
+    members:
+    {
+      takimototaro: true,
+      hisakatarei: true
+    }
+  },
+  {
+    name: 'オレたちは双子じゃない',
+    members:
+    {
+      mitumorisakyo: true,
+      mitumoriukyo: true
+    }
+  },
+  {
+    name: '究極VS至高',
+    members:
+    {
+      tachibanamizuki: true,
+      rikudouhijiri: true
+    }
+  },
+  {
+	  name: '兄弟の事情',
+    members:
+    {
+      ikarimamoru: true,
+      ikarisusumu: true
+    }
+  },
+  {
+    name: 'ひまわりと月見草',
+    members:
+    {
+      shibutanihideki: true,
+      nakanoshimayukihiro: true
+    }
+  },
+  {
+    name: '野球vsソフトボール',
+    members:
+    {
+      bitochihiro: true,
+      kawahoshihomura: true
+    }
+  },
+  {
+    name: 'NGワード',
+    members:
+    {
+      hoshiisubaru: true,
+      saigayuto: true
+    }
+  },
+  {
+    name: 'エリートと秀才',
+    members:
+    {
+      hoshiisubaru: true,
+      karasumatakemitsu: true
+    }
+  },
+  {
+    name: 'クセモノズ',
+    members:
+    {
+      odagiritakumi: true,
+      kodairahinata: true
+    }
+  },
+  {
+    name: '意外な2人',
+    members:
+    {
+      udomikihisa: true,
+      karasumatakemitsu: true
+    }
+  },
+  {
+    name: '投手討論',
+    members:
+    {
+      hayakawaaoi: true,
+      tachikawahiromi: true
+    }
+  },
+  {
+    name: '猛特訓?',
+    members:
+    {
+      tomosawaryo: true,
+      hebishimakirito: true
+    }
+  },
+  {
+    name: 'アイツには負けない!',
+    members:
+    {
+      tomosawaryo: true,
+      ikarimamoru: true
+    }
+  },
+  {
+    name: '最強の特訓メニュー',
+    members:
+    {
+      tomosawaryo: true,
+      turuyamasaru: true
+    }
+  },
+  {
+    name: 'ストイックツー',
+    members:
+    {
+      tomosawaryo: true,
+      zenigataranzo: true
+    }
+  },
+  {
+    name: 'たこ燒きパーティー',
+    members:
+    {
+      abatayasushi: true,
+      tsukumotakaoki: true
+    }
+  },
+  {
+    name: 'リベラルな捕手',
+    members:
+    {
+      rikudouhijiri: true,
+      kodakamirei: true
+    }
+  },
+  {
+    name: 'ニコイチ',
+    members:
+    {
+      tachikawahiromi: true,
+      kodakamirei: true
+    }
+  },
+  {
+    name: 'トクベツな関係',
+    members:
+    {
+      bitochihiro: true,
+      kodakamirei: true
+    }
+  },
+  {
+    name: '美藤vsアンヌ',
+    members:
+    {
+      annu: true,
+      bitochihiro: true
+    }
+  },
+  {
+    name: 'ボケ合戦',
+    members:
+    {
+      akehoshisetuka: true,
+      hasshakumikoko: true
+    }
+  },
+  {
+    name: 'チャレンジ精神',
+    members:
+    {
+      akehoshisetuka: true,
+      misonochika: true
+    }
+  },
+  {
+    name: 'あばれ八尺と小旋風',
+    members:
+    {
+      koarashiryo: true,
+      hasshakumikoko: true
+    }
+  },
+  {
+    name: 'ケンカするほど仲がいい?',
+    members:
+    {
+      koarashiryo: true,
+      misonochika: true,
+    }
+  },
+  {
+    name: '女性という生き方',
+    members:
+    {
+      natunohimawari: true,
+      kodakamirei: true
+    }
+  },
+  {
+    name: 'スイーツ番長',
+    members:
+    {
+      natunohimawari: true,
+      yabetaakiko: true
+    }
+  },
+  {
+    name: 'テリブルトリオ',
+    members:
+    {
+      hayakawaaoi: true,
+      tachibanamizuki: true,
+      rikudouhijiri: true
+    }
+  },
+  {
+    name: 'セレブなレストラン',
+    members:
+    {
+      ikarimamoru: true,
+      ookanemochitaro: true,
+      tachibanamizuki: true
+    }
+  },
+  {
+    name: '憧れです!',
+    members:
+    {
+      annu: true,
+      tachikawahiromi: true,
+      hayakawaaoi: true
+    }
+  },
+  {
+    name: 'ハイパートレーニング',
+    members:
+    {
+      tomosawaryo: true,
+      charaigenki: true,
+      tachikawahiromi: true
+    }
+  },
+  {
+    name: '女子パワー',
+    members:
+    {
+      tachibanamizuki: true,
+      rikudouhijiri: true,
+      tachikawahiromi: true,
+      kodakamirei: true
+    }
+  },
+  {
+    name: 'みんなでビーチバレー',
+    members:
+    {
+      akehoshisetuka: true,
+      hasshakumikoko: true,
+      koarashiryo: true,
+      misonochika: true
+    }
+  }
+
 ];
 
 
